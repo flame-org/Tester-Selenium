f108b07a898e9ad6cc68f17defec9a9b6a612eab
JSifalda <sifalda.jiri@gmail.com>
2013-06-26 19:43:39 +0200

    Fix path: move to bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/../libs/autoload.php';
+require __DIR__ . '/../libs/facebook/php-webdriver/__init__.php';
 
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
